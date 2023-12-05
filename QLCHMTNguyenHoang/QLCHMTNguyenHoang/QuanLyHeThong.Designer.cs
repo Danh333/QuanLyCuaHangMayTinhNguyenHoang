488cf1e0f7e7c83b977f28311b9f442e11aacb11
Thuan7723 <thuan021101065@tgu.edu.vn>
2023-12-05 08:20:42 +0700

    abc

--- QLCHMTNguyenHoang/QLCHMTNguyenHoang/QuanLyHeThong.Designer.cs
+++ QLCHMTNguyenHoang/QLCHMTNguyenHoang/QuanLyHeThong.Designer.cs
@@ -32,6 +32,9 @@
             this.button1 = new System.Windows.Forms.Button();
             this.label1 = new System.Windows.Forms.Label();
             this.label2 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button2
@@ -74,11 +77,41 @@
             this.label2.Size = new System.Drawing.Size(0, 14);
             this.label2.TabIndex = 6;
             // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(433, 215);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(118, 46);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(283, 215);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(118, 46);
+            this.button4.TabIndex = 8;
+            this.button4.Text = "button4";
+            this.button4.UseVisualStyleBackColor = true;
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(105, 215);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(118, 46);
+            this.button5.TabIndex = 9;
+            this.button5.Text = "button5";
+            this.button5.UseVisualStyleBackColor = true;
+            // 
             // QuanLyHeThong
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 12F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(947, 422);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.button1);
@@ -97,5 +130,8 @@
         private System.Windows.Forms.Button button1;
         private System.Windows.Forms.Label label1;
         private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
     }
 }
