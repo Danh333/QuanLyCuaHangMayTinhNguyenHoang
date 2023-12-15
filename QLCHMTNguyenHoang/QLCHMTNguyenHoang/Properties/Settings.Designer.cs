bb13c5283acc3bda5a3439be9314d3eaab2a04f7
Anh Minh <Anh Minh@WHOANHMINH>
2023-12-15 09:57:51 +0700

    them xuất hd

--- QLCHMTNguyenHoang/QLCHMTNguyenHoang/Properties/Settings.Designer.cs
+++ QLCHMTNguyenHoang/QLCHMTNguyenHoang/Properties/Settings.Designer.cs
@@ -12,7 +12,7 @@ namespace QLCHMTNguyenHoang.Properties {
     
     
     [global::System.Runtime.CompilerServices.CompilerGeneratedAttribute()]
-    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "17.7.0.0")]
+    [global::System.CodeDom.Compiler.GeneratedCodeAttribute("Microsoft.VisualStudio.Editors.SettingsDesigner.SettingsSingleFileGenerator", "16.10.0.0")]
     internal sealed partial class Settings : global::System.Configuration.ApplicationSettingsBase {
         
         private static Settings defaultInstance = ((Settings)(global::System.Configuration.ApplicationSettingsBase.Synchronized(new Settings())));
@@ -30,6 +30,17 @@ namespace QLCHMTNguyenHoang.Properties {
         public string abcConnectionString {
             get {
                 return ((string)(this["abcConnectionString"]));
+            }
+        }
+        
+        [global::System.Configuration.ApplicationScopedSettingAttribute()]
+        [global::System.Diagnostics.DebuggerNonUserCodeAttribute()]
+        [global::System.Configuration.SpecialSettingAttribute(global::System.Configuration.SpecialSetting.ConnectionString)]
+        [global::System.Configuration.DefaultSettingValueAttribute("Data Source=whoanhminh\\sqlexpress;Initial Catalog=quanli123;Integrated Security=T" +
+            "rue")]
+        public string quanli123ConnectionString {
+            get {
+                return ((string)(this["quanli123ConnectionString"]));
             }
         }
     }
