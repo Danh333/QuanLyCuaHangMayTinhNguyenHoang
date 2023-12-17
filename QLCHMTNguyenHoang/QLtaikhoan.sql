2011e45aba93f927f70845630cf586a4e07de1ce
Khanh <Khanh021101109@tgu.edu.vn>
2023-12-17 11:45:22 +0700

    QLNV

--- QLCHMTNguyenHoang/QLtaikhoan.sql
+++ QLCHMTNguyenHoang/QLtaikhoan.sql
@@ -2,7 +2,7 @@ CREATE DATABASE QLtaikhoan
 USE QLtaikhoan
 GO
 
-/****** Object:  Table [dbo].[nguoidung]    Script Date: 12/15/2023 9:22:22 AM ******/
+/****** Object:  Table [dbo].[nguoidung]    Script Date: 12/12/2023 9:22:22 AM ******/
 SET ANSI_NULLS ON
 GO
 
@@ -17,5 +17,6 @@ CREATE TABLE [dbo].[nguoidung](
 
 GO
 
-Insert into nguoidung(username,password,quyen) values ('admin','12345','admin')
+Insert into nguoidung(username,password,quyen) values ('admin','12345','admin');
+Insert into nguoidung(username,password,quyen) values ('Danh','12345',N'nhân viên kho hàng');
 
